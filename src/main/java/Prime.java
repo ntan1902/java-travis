9d9b96775129eaaa3c77c266b3262d0ec0bd7ed0
annt12 <annt12@vng.com.vn>
2021-06-17 21:16:44 +0700

    Test fail

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -1,7 +1,7 @@
 public class Prime {
     boolean checkPrime(int n) {
         if (n < 2) return false;
-        for (int i = 2; i * i <= n; i++) {
+        for (int i = 2; i * i < n; i++) {
             if (n % i == 0) {
                 return false;
             }
